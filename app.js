8c7b0777d98770359506ab93fe452e8ac193edf7
Alex Widener <awidener3@gmail.com>
2021-10-26 13:21:58 -0400

    fixed logic with reset button

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+let x = 16;
 const container_div = document.getElementById('container');
 const reset_button = document.getElementById('reset-button');
 const gridpiece_div = document.getElementsByClassName('grid-piece');
 
-function addGrid(columns, rows) {
-    for (let i = 0; i < columns; i++) {
+function createGrid(x) {
+    for (let i = 0; i < x; i++) {
         const div = document.createElement('div');
-        // div.classList.add('grid-piece');
         div.classList.add('column-'+i);
         container_div.appendChild(div);
-        for (let j = 0; j < rows; j++) {
+        for (let j = 0; j < x; j++) {
             const div = document.createElement('div');
+            div.style.width = `${(750/x)}px`;
+            div.style.height = `${(750/x)}px`;
             div.classList.add('grid-piece');
             div.classList.add('column-'+i);
             div.classList.add('row-'+j);
@@ -32,10 +34,28 @@ function resetClass() {
     }
 }
 
-function main() {
-    reset_button.addEventListener('click', () => resetClass())
-    addGrid(16, 16);
+function resetGrid() {
+    const pieces = document.getElementsByClassName('grid-piece');
+    while(pieces.length > 0) {
+        pieces[0].parentNode.removeChild(pieces[0]);
+    }
+}
+
+function startPage () {
+    createGrid(x);
     drawOnCanvas();
 }
 
-main();
+startPage();
+reset_button.addEventListener('click', function() {
+    // resetClass()
+    resetGrid();
+    let x = prompt('how big would you like the canvas to be on each side?');
+    if (x >= 100) {
+        alert('number must be less than 100!')
+        return;
+    } else {
+        createGrid(x);
+        drawOnCanvas();
+    }
+})
